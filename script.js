3e03d1be49e50a963d36df0f7ff6428eec199b2d
Aleksandr Hrytsenko <alkatraz0704@gmail.com>
2019-12-30 00:50:47 +0200

    Create first little app number randomizer

---
+++ script.js
@@ -0,0 +1,27 @@
+var buttonGenerate = document.getElementById("btnGen");
+var buttonReset = document.getElementById("btnRes");
+
+
+
+
+buttonGenerate.addEventListener('click',function ()  {
+    let max = document.getElementById("upperBound").value;
+    let min = document.getElementById("underBound").value;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    document.getElementById("resultValue").value = Math.floor(Math.random() * (max - min + 1)) + min;
+});
+
+
+buttonReset.addEventListener('click', function(){
+    document.getElementById("upperBound").value = "";
+    document.getElementById("underBound").value = "";
+    document.getElementById("resultValue").value = "";
+});
+
+
+
+
+
+
+
