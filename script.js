66708ec21cac46028e9bd20d4cc27ad2bab268fd
Aleksandr Hrytsenko <alkatraz0704@gmail.com>
2021-06-18 14:18:21 +0300

    delete empty lines

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 var buttonGenerate = document.getElementById("btnGen");
 var buttonReset = document.getElementById("btnRes");
 
-
-
-
 buttonGenerate.addEventListener('click',function ()  {
     let max = document.getElementById("upperBound").value;
     let min = document.getElementById("underBound").value;
@@ -12,16 +9,8 @@ buttonGenerate.addEventListener('click',function ()  {
     document.getElementById("resultValue").value = Math.floor(Math.random() * (max - min + 1)) + min;
 });
 
-
 buttonReset.addEventListener('click', function(){
     document.getElementById("upperBound").value = "";
     document.getElementById("underBound").value = "";
     document.getElementById("resultValue").value = "";
 });
-
-
-
-
-
-
-
